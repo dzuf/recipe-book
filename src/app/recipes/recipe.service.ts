cc985f405f8b265913dc76a5450c90c76dd3ec39
Davor Zufika <davor.zufika@ecx.io>
2021-05-18 14:10:28 +0200

    Added routing

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -32,6 +32,10 @@ export class RecipeService {
     return this.recipes.slice();
   }
 
+  getRecipe(index: number): any {
+    return this.recipes[index];
+  }
+
   addIngredientsToShoppingList(ingredients: Ingredient[]): void {
     this.slService.addIngredients(ingredients);
   }
